02976b8eb76fc7ce7a47828707054a7d976146ff
max <maxsaintadjutor@gmail.com>
2023-11-02 18:02:30 +0100

    Ajout du fichier App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,4 @@
-import React, { Component } from 'react';
-import './App.css';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      imageUrl: '', // Stocker l'URL de l'image
-    };
-  }
-
-  // Gérer le changement dans la zone de texte
-  handleInputChange = (event) => {
-    this.setState({ imageUrl: event.target.value });
-  }
-
-  // Gérer le clic sur le bouton d'analyse d'image
-  handleAnalyzeClick = () => {
-    // Ajoutez ici la logique pour l'analyse d'image (dans les exercices suivants)
-    console.log('Analyse d\'image déclenchée');
-  }
-
-  // Gérer le clic sur le bouton de génération d'image
-  handleGenerateClick = () => {
-    // Ajoutez ici la logique pour la génération d'image (dans les exercices suivants)
-    console.log('Génération d\'image déclenchée');
-  }
-
-  render() {
-    return (
+  return (
       <div className="App">
         <h1>Application de Vision par Ordinateur</h1>
         <label htmlFor="imageUrl">URL de l'image :</label>
@@ -45,3 +16,4 @@ class App extends Component {
 }
 
 export default App;
+
